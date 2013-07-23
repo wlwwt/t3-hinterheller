64bb3e3b57afbf3249285732df67bd10d1d5208d
Wolfgang Freund <wolfgang.freund@cyberhouse.at>
2013-07-23 17:43:35 +0200

    production settings

--- ext/theme_hinterheller/Resources/Private/TypoScript/constants-production.ts
+++ ext/theme_hinterheller/Resources/Private/TypoScript/constants-production.ts
@@ -1,17 +1,8 @@
-# ******************************************************************************
-#	(c) 2012 Georg Ringer <typo3@ringerge.org>
-#
-#	You can redistribute it and/or modify it under the terms of the
-#	GNU General Public License as published by the Free Software Foundation;
-#	either version 2 of the License, or (at your option) any later version.
-# ******************************************************************************
-
-
 # **********************************************************
 # Constants of the PRODUCTION mode
 # **********************************************************
 
-plugin.theme_configuration.url = http://www.sv-hinterheller/
+plugin.theme_configuration.url = http://www.sv-hinterheller.de/
 
 plugin.theme_configuration.general.mail.link = info@sv-hinterheller.de
 plugin.theme_configuration.general.url.link = http://www.sv-hinterheller.de
@@ -22,4 +13,18 @@ plugin.theme_configuration.navigation_small = 2,11
 plugin.theme_configuration.assets.compress = 1
 plugin.theme_configuration.assets.merge = 1
 
+# **********************************************************
+# Changes in styles
+# **********************************************************
+styles.content.imgtext.maxW = 800
+styles.content.imgtext.maxH = 800
+
+# **********************************************************
+# Enable realurl
+# **********************************************************
+plugin.theme_configuration.realurl = 1
+
+# **********************************************************
+# Google sitemap start pid
+# **********************************************************
 plugin.theme_configuration.google.sitemap = 2
